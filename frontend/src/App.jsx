59dcd155cece72d33edc024c593181cfd29f5056
ALI KHUSROO BIN SABIR <alisabir97570@gmail.com>
2026-04-05 17:34:15 +0530

    Fix case-sensitivity in SignUp import for Vercel build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
-import Signup from './pages/Signup'
+import SignUp from './pages/SignUp'
 import LandingPage from './pages/LandingPage'
 import Dashboard from './pages/Dashboard'
 import InterviewPrep from './pages/InterviewPrep'
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/interview/:id" element={<InterviewPrep />} />
